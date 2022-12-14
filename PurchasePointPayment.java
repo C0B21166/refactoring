cd2c0fd74eb80070a11e6b2ed963eceae7778e6f
C0B21166 <c0b21166df@edu.teu.ac.jp>
2022-12-14 10:16:33 +0900

    リファクタリングした

--- PurchasePointPayment.java
+++ PurchasePointPayment.java
@@ -1,30 +1,23 @@
 import java.time.LocalDateTime;
 
 class PurchasePointPayment {
-  final CustomerId customerId;          // 購入者のID
-  final ComicId comicId;                // 購入するWebコミックのID
-  final PurchasePoint consumptionPoint; // 購入で消費するポイント
-  final LocalDateTime paymentDateTime;  // 購入日時
-
-  PurchasePointPayment(final Customer customer, final Comic comic) {
-    if (customer.isEnabled()) {
-      customerId = customer.id;
-      if (comic.isEnabled()) {
+    final CustomerId customerId; // 購入者のID
+    final ComicId comicId; // 購入するWebコミックのID
+    final PurchasePoint consumptionPoint; // 購入で消費するポイント
+    final LocalDateTime paymentDateTime; // 購入日時
+    PurchasePointPayment(final Customer customer, final Comic comic) {
+        if (customer.isDisabled()) {
+            throw new IllegalArgumentException("有効な購入者ではありません。");
+        }
+        if (comic.isDisabled()) {
+            throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
+        }
+        if (customer.isShortOfPoint(comic)) {
+            throw new RuntimeException("所持ポイントが不足しています。");
+        }
+        customerId = customer.id;
         comicId = comic.id;
-        if (comic.currentPurchasePoint.amount <= customer.possessionPoint.amount) {
-          consumptionPoint = comic.currentPurchasePoint;
-          paymentDateTime = LocalDateTime.now();
-        }
-        else {
-          throw new RuntimeException("所持ポイントが不足しています。");
-        }
-      }
-      else {
-        throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
-      }
+        consumptionPoint = comic.currentPurchasePoint;
+        paymentDateTime = LocalDateTime.now();
     }
-    else {
-      throw new IllegalArgumentException("有効な購入者ではありません。");
-    }
-  }
 }
